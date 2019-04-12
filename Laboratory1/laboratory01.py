7aceea8336d1eddc0a2a65333e53d3cb53757f9c
Marcos Vinicius <marcos.v.silva@live.com>
2019-04-12 17:29:44 -0300

    update lab01

--- Laboratory1/laboratory01.py
+++ Laboratory1/laboratory01.py
@@ -1,18 +1,44 @@
+import numpy as np
 import cv2
 
 
-def print_image(img):
-    cv2.imshow('show_img', img)
+def print_image(name_image, image):
+    cv2.imshow(name_image, image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
 
-def invert(img):
-    return abs(255 - img)
+def convert_image_to_RGB(image):
+    height, width = image.shape
+    channels = 3
+    img_rgb = np.zeros((height, width, channels))
+
+    for ch in range(channels):
+        for he in range(height):
+            for wi in range(width):
+                img_rgb[he, wi, ch] = img[ch, wi]
+
+    return img_rgb
 
 
-img_original = cv2.imread('mammography2.png')
-#print_image(img_original)
+def image_to_gray_scale(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-image_invert = invert(img_original)
-print_image(image_invert)
+
+def invert(image):
+    return abs(255 - image)
+
+
+# Load image and convert image to RGB
+img = np.loadtxt('ImageMatriz.txt')
+img = convert_image_to_RGB(img)
+img = img.astype('uint8')
+
+# Print original image
+print_image('original', img)
+
+# Convert to gray scale
+img_gray = image_to_gray_scale(img)
+
+# Print gray image
+print_image('gray scale', img_gray)
