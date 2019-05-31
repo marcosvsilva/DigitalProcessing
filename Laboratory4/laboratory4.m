497043bad0da04171036f8cc1eef27c8bfc2ce93
Marcos Vinicius <marcos.v.silva@live.com>
2019-05-31 18:41:53 -0300

    Finishing exercise 5

--- Laboratory4/laboratory4.m
+++ Laboratory4/laboratory4.m
@@ -232,7 +232,7 @@ subplot(3,4,12); imshow(img_erode); title(img_title);
 % Variar o tamanho do elemento estruturante de 5 a 15 e
 % observar os resultados.
 
-% Lendo a imagem original e convertendo em binária
+% Lendo a imagem original
 img_ori = imread('Laboratorio_4_4.tif');
 
 % Criando imagem do exercício 4 - Abertura e Fechamento Quadrado de 5 a 8
@@ -259,9 +259,9 @@ structuring_element = strel('square', 6);
 img_open = imopen(img_ori, structuring_element);
 img_close = imclose(img_ori, structuring_element);
 
-img_title = 'Abertura quadrado 6';
+img_title = 'Abertura por quadrado 6';
 subplot(3,5,9); imshow(img_open); title(img_title);
-img_title = 'Fechamento quadrado 6';
+img_title = 'Fechamento por quadrado 6';
 subplot(3,5,10); imshow(img_close); title(img_title);
 
 % 3- Elemento tipo quadrado de tamanho 7
@@ -270,9 +270,9 @@ structuring_element = strel('square', 7);
 img_open = imopen(img_ori, structuring_element);
 img_close = imclose(img_ori, structuring_element);
 
-img_title = 'Abertura quadrado 7';
+img_title = 'Abertura por quadrado 7';
 subplot(3,5,11); imshow(img_open); title(img_title);
-img_title = 'Fechamento quadrado 7';
+img_title = 'Fechamento por quadrado 7';
 subplot(3,5,12); imshow(img_close); title(img_title);
 
 % 4- Elemento tipo quadrado de tamanho 8
@@ -281,9 +281,9 @@ structuring_element = strel('square', 8);
 img_open = imopen(img_ori, structuring_element);
 img_close = imclose(img_ori, structuring_element);
 
-img_title = 'Abertura quadrado 8';
+img_title = 'Abertura por quadrado 8';
 subplot(3,5,14); imshow(img_open); title(img_title);
-img_title = 'Fechamento quadrado 8';
+img_title = 'Fechamento por quadrado 8';
 subplot(3,5,15); imshow(img_close); title(img_title);
 
 % Criando imagem do exercício 4 - Abertura e Fechamento Quadrado de 9 a 12
@@ -299,9 +299,9 @@ structuring_element = strel('square', 9);
 img_open = imopen(img_ori, structuring_element);
 img_close = imclose(img_ori, structuring_element);
 
-img_title = 'Abertura quadrado 9';
+img_title = 'Abertura por quadrado 9';
 subplot(3,5,6); imshow(img_open); title(img_title);
-img_title = 'Fechamento quadrado 9';
+img_title = 'Fechamento por quadrado 9';
 subplot(3,5,7); imshow(img_close); title(img_title);
 
 % 6- Elemento tipo quadrado de tamanho 10
@@ -310,9 +310,9 @@ structuring_element = strel('square', 10);
 img_open = imopen(img_ori, structuring_element);
 img_close = imclose(img_ori, structuring_element);
 
-img_title = 'Abertura quadrado 10';
+img_title = 'Abertura por quadrado 10';
 subplot(3,5,9); imshow(img_open); title(img_title);
-img_title = 'Fechamento quadrado 10';
+img_title = 'Fechamento por quadrado 10';
 subplot(3,5,10); imshow(img_close); title(img_title);
 
 % 7- Elemento tipo quadrado de tamanho 11
@@ -321,9 +321,9 @@ structuring_element = strel('square', 11);
 img_open = imopen(img_ori, structuring_element);
 img_close = imclose(img_ori, structuring_element);
 
-img_title = 'Abertura quadrado 11';
+img_title = 'Abertura por quadrado 11';
 subplot(3,5,11); imshow(img_open); title(img_title);
-img_title = 'Fechamento quadrado 11';
+img_title = 'Fechamento por quadrado 11';
 subplot(3,5,12); imshow(img_close); title(img_title);
 
 % 8- Elemento tipo quadrado de tamanho 12
@@ -332,9 +332,9 @@ structuring_element = strel('square', 12);
 img_open = imopen(img_ori, structuring_element);
 img_close = imclose(img_ori, structuring_element);
 
-img_title = 'Abertura quadrado 12';
+img_title = 'Abertura por quadrado 12';
 subplot(3,5,14); imshow(img_open); title(img_title);
-img_title = 'Fechamento quadrado 12';
+img_title = 'Fechamento por quadrado 12';
 subplot(3,5,15); imshow(img_close); title(img_title);
 
 % Criando imagem do exercício 4 - Abertura e Fechamento Quadrado de 13 a 16
@@ -350,9 +350,9 @@ structuring_element = strel('square', 13);
 img_open = imopen(img_ori, structuring_element);
 img_close = imclose(img_ori, structuring_element);
 
-img_title = 'Abertura quadrado 13';
+img_title = 'Abertura por quadrado 13';
 subplot(3,5,6); imshow(img_open); title(img_title);
-img_title = 'Fechamento quadrado 13';
+img_title = 'Fechamento por quadrado 13';
 subplot(3,5,7); imshow(img_close); title(img_title);
 
 % 10- Elemento tipo quadrado de tamanho 14
@@ -361,9 +361,9 @@ structuring_element = strel('square', 14);
 img_open = imopen(img_ori, structuring_element);
 img_close = imclose(img_ori, structuring_element);
 
-img_title = 'Abertura quadrado 14';
+img_title = 'Abertura por quadrado 14';
 subplot(3,5,9); imshow(img_open); title(img_title);
-img_title = 'Fechamento quadrado 14';
+img_title = 'Fechamento por quadrado 14';
 subplot(3,5,10); imshow(img_close); title(img_title);
 
 % 11- Elemento tipo quadrado de tamanho 15
@@ -372,9 +372,9 @@ structuring_element = strel('square', 15);
 img_open = imopen(img_ori, structuring_element);
 img_close = imclose(img_ori, structuring_element);
 
-img_title = 'Abertura quadrado 15';
+img_title = 'Abertura por quadrado 15';
 subplot(3,5,11); imshow(img_open); title(img_title);
-img_title = 'Fechamento quadrado 15';
+img_title = 'Fechamento por quadrado 15';
 subplot(3,5,12); imshow(img_close); title(img_title);
 
 % 11- Elemento tipo quadrado de tamanho 16
@@ -383,162 +383,215 @@ structuring_element = strel('square', 16);
 img_open = imopen(img_ori, structuring_element);
 img_close = imclose(img_ori, structuring_element);
 
-img_title = 'Abertura quadrado 16';
+img_title = 'Abertura por quadrado 16';
 subplot(3,5,14); imshow(img_open); title(img_title);
-img_title = 'Fechamento quadrado 16';
+img_title = 'Fechamento por quadrado 16';
 subplot(3,5,15); imshow(img_close); title(img_title);
 
 % Alterando o elemento estruturante para disco
 
-% Criando imagem do exercício 4 - Abertura e Fechamento Disco de 5 a 8
+% Criando imagem do exercício 4 - Abertura e Fechamento disco de 5 a 8
 title_figure = 'Exercicio 4 - Abertura e Fechamento Disco de 5 a 8';
 figure('NumberTitle', 'off', 'Name', title_figure);
 
 % Exibindo a imagem original
 subplot(3,5,[1 5]); imshow(img_ori); title('Imagem original');
 
-% 1- Elemento tipo Disco de tamanho 5
+% 1- Elemento tipo disco de tamanho 5
 structuring_element = strel('disk', 5);
 
 img_open = imopen(img_ori, structuring_element);
 img_close = imclose(img_ori, structuring_element);
 
-img_title = 'Abertura por Disco 5';
+img_title = 'Abertura por disco 5';
 subplot(3,5,6); imshow(img_open); title(img_title);
-img_title = 'Fechamento por Disco 5';
+img_title = 'Fechamento por disco 5';
 subplot(3,5,7); imshow(img_close); title(img_title);
 
-% 2- Elemento tipo Disco de tamanho 6
+% 2- Elemento tipo disco de tamanho 6
 structuring_element = strel('disk', 6);
 
 img_open = imopen(img_ori, structuring_element);
 img_close = imclose(img_ori, structuring_element);
 
-img_title = 'Abertura Disco 6';
+img_title = 'Abertura por disco 6';
 subplot(3,5,9); imshow(img_open); title(img_title);
-img_title = 'Fechamento Disco 6';
+img_title = 'Fechamento por disco 6';
 subplot(3,5,10); imshow(img_close); title(img_title);
 
-% 3- Elemento tipo Disco de tamanho 7
+% 3- Elemento tipo disco de tamanho 7
 structuring_element = strel('disk', 7);
 
 img_open = imopen(img_ori, structuring_element);
 img_close = imclose(img_ori, structuring_element);
 
-img_title = 'Abertura Disco 7';
+img_title = 'Abertura por disco 7';
 subplot(3,5,11); imshow(img_open); title(img_title);
-img_title = 'Fechamento Disco 7';
+img_title = 'Fechamento por disco 7';
 subplot(3,5,12); imshow(img_close); title(img_title);
 
-% 4- Elemento tipo Disco de tamanho 8
+% 4- Elemento tipo disco de tamanho 8
 structuring_element = strel('disk', 8);
 
 img_open = imopen(img_ori, structuring_element);
 img_close = imclose(img_ori, structuring_element);
 
-img_title = 'Abertura Disco 8';
+img_title = 'Abertura por disco 8';
 subplot(3,5,14); imshow(img_open); title(img_title);
-img_title = 'Fechamento Disco 8';
+img_title = 'Fechamento por disco 8';
 subplot(3,5,15); imshow(img_close); title(img_title);
 
-% Criando imagem do exercício 4 - Abertura e Fechamento Disco de 9 a 12
+% Criando imagem do exercício 4 - Abertura e Fechamento disco de 9 a 12
 title_figure = 'Exercicio 4 - Abertura e Fechamento Disco de 9 a 12';
 figure('NumberTitle', 'off', 'Name', title_figure);
 
 % Exibindo a imagem original
 subplot(3,5,[1 5]); imshow(img_ori); title('Imagem original');
 
-% 5- Elemento tipo Disco de tamanho 9
+% 5- Elemento tipo disco de tamanho 9
 structuring_element = strel('disk', 9);
 
 img_open = imopen(img_ori, structuring_element);
 img_close = imclose(img_ori, structuring_element);
 
-img_title = 'Abertura Disco 9';
+img_title = 'Abertura por disco 9';
 subplot(3,5,6); imshow(img_open); title(img_title);
-img_title = 'Fechamento Disco 9';
+img_title = 'Fechamento por disco 9';
 subplot(3,5,7); imshow(img_close); title(img_title);
 
-% 6- Elemento tipo Disco de tamanho 10
+% 6- Elemento tipo disco de tamanho 10
 structuring_element = strel('disk', 10);
 
 img_open = imopen(img_ori, structuring_element);
 img_close = imclose(img_ori, structuring_element);
 
-img_title = 'Abertura Disco 10';
+img_title = 'Abertura por disco 10';
 subplot(3,5,9); imshow(img_open); title(img_title);
-img_title = 'Fechamento Disco 10';
+img_title = 'Fechamento por disco 10';
 subplot(3,5,10); imshow(img_close); title(img_title);
 
-% 7- Elemento tipo Disco de tamanho 11
+% 7- Elemento tipo disco de tamanho 11
 structuring_element = strel('disk', 11);
 
 img_open = imopen(img_ori, structuring_element);
 img_close = imclose(img_ori, structuring_element);
 
-img_title = 'Abertura Disco 11';
+img_title = 'Abertura por disco 11';
 subplot(3,5,11); imshow(img_open); title(img_title);
-img_title = 'Fechamento Disco 11';
+img_title = 'Fechamento por disco 11';
 subplot(3,5,12); imshow(img_close); title(img_title);
 
-% 8- Elemento tipo Disco de tamanho 12
+% 8- Elemento tipo disco de tamanho 12
 structuring_element = strel('disk', 12);
 
 img_open = imopen(img_ori, structuring_element);
 img_close = imclose(img_ori, structuring_element);
 
-img_title = 'Abertura Disco 12';
+img_title = 'Abertura por disco 12';
 subplot(3,5,14); imshow(img_open); title(img_title);
-img_title = 'Fechamento Disco 12';
+img_title = 'Fechamento por disco 12';
 subplot(3,5,15); imshow(img_close); title(img_title);
 
-% Criando imagem do exercício 4 - Abertura e Fechamento Disco de 13 a 16
+% Criando imagem do exercício 4 - Abertura e Fechamento disco de 13 a 16
 title_figure = 'Exercicio 4 - Abertura e Fechamento Disco de 13 a 16';
 figure('NumberTitle', 'off', 'Name', title_figure);
 
 % Exibindo a imagem original
 subplot(3,5,[1 5]); imshow(img_ori); title('Imagem original');
 
-% 9- Elemento tipo Disco de tamanho 13
+% 9- Elemento tipo disco de tamanho 13
 structuring_element = strel('disk', 13);
 
 img_open = imopen(img_ori, structuring_element);
 img_close = imclose(img_ori, structuring_element);
 
-img_title = 'Abertura Disco 13';
+img_title = 'Abertura por disco 13';
 subplot(3,5,6); imshow(img_open); title(img_title);
-img_title = 'Fechamento Disco 13';
+img_title = 'Fechamento por disco 13';
 subplot(3,5,7); imshow(img_close); title(img_title);
 
-% 10- Elemento tipo Disco de tamanho 14
+% 10- Elemento tipo disco de tamanho 14
 structuring_element = strel('disk', 14);
 
 img_open = imopen(img_ori, structuring_element);
 img_close = imclose(img_ori, structuring_element);
 
-img_title = 'Abertura Disco 14';
+img_title = 'Abertura por disco 14';
 subplot(3,5,9); imshow(img_open); title(img_title);
-img_title = 'Fechamento Disco 14';
+img_title = 'Fechamento por disco 14';
 subplot(3,5,10); imshow(img_close); title(img_title);
 
-% 11- Elemento tipo Disco de tamanho 15
+% 11- Elemento tipo disco de tamanho 15
 structuring_element = strel('disk', 15);
 
 img_open = imopen(img_ori, structuring_element);
 img_close = imclose(img_ori, structuring_element);
 
-img_title = 'Abertura Disco 15';
+img_title = 'Abertura por disco 15';
 subplot(3,5,11); imshow(img_open); title(img_title);
-img_title = 'Fechamento Disco 15';
+img_title = 'Fechamento por disco 15';
 subplot(3,5,12); imshow(img_close); title(img_title);
 
-% 11- Elemento tipo Disco de tamanho 16
+% 11- Elemento tipo disco de tamanho 16
 structuring_element = strel('disk', 16);
 
 img_open = imopen(img_ori, structuring_element);
 img_close = imclose(img_ori, structuring_element);
 
-img_title = 'Abertura Disco 16';
+img_title = 'Abertura disco 16';
 subplot(3,5,14); imshow(img_open); title(img_title);
-img_title = 'Fechamento Disco 16';
+img_title = 'Fechamento disco 16';
 subplot(3,5,15); imshow(img_close); title(img_title);
+
+%--------------------------------------------
+% Exercício 5
+%--------------------------------------------
+
+% Utilizando a imagem Laboratorio_4_5.tif, realizar a operação de
+% abertura e fechamento com um elemento estruturante do tipo
+% Quadrado de tamanho 2. Repetir a operação com um elemento
+% estruturante do tipo Disco de tamanho 2 e observar os resultados.
+
+% Lendo a imagem original
+img_ori = imread('Laboratorio_4_5.tif');
+
+% Criando imagem do exercício 5
+title_figure = 'Exercicio 5';
+figure('NumberTitle', 'off', 'Name', title_figure);
+
+% Exibindo a imagem original
+subplot(2,5,[1 5]); imshow(img_ori); title('Imagem original');
+
+% 1- Elemento tipo quadrado de tamanho 2
+structuring_element = strel('square', 2);
+
+img_open = imopen(img_ori, structuring_element);
+img_close = imclose(img_ori, structuring_element);
+
+img_title = 'Abertura por quadrado 2';
+subplot(2,5,6); imshow(img_open); title(img_title);
+img_title = 'Fechamento por quadrado 2';
+subplot(2,5,7); imshow(img_close); title(img_title);
+
+% 2- Elemento tipo disco de tamanho 2
+structuring_element = strel('disk', 2);
+
+img_open = imopen(img_ori, structuring_element);
+img_close = imclose(img_ori, structuring_element);
+
+img_title = 'Abertura por disco 2';
+subplot(2,5,9); imshow(img_open); title(img_title);
+img_title = 'Fechamento por disco 2';
+subplot(2,5,10); imshow(img_close); title(img_title);
+
+% Comentário
+
+% Podemos observar que na figura original possui diversos ruídos
+% circulares junto à impressão digital que fazem que a impressão
+% digital seja bastante desfalcada na tentativa de ocultação destes
+% ruídos com a abertura em estrutura de disco e bastante ressaltadas
+% no fechamento com a mesma estrutura. Ao redor da impressão digital
+% existe bastante ruído em outros formatos que são bem ocultos na
+% abertura da imagem com estrutura de quadrado e bastante ressaltadas 
+% com o fechamento em estrutura de quadrado, no entanto, não possui
+% resultados satisfatórios com os ruídos circulares ao centro da digital.
