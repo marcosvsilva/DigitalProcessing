750ed78cf9849f9e392fd979809ab9c83e578a53
Marcos Vinicius <marcos.v.silva@live.com>
2019-06-14 16:53:37 -0300

    Init laboratory5

--- Laboratory5/Laboratory5.m
+++ Laboratory5/Laboratory5.m
@@ -1,1 +1,8 @@
-init
+%--------------------------------------------
+% Laboratório 5
+% Aluno: Marcos Vinicius Ribeiro Silva
+%--------------------------------------------
+
+% Adicionando o path onde contêm as imagens
+clc;
+addpath('Images');
