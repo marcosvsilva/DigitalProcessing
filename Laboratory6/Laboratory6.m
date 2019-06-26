55c21f7f48f59e325b502296bca93755af71233c
Marcos Vinicius <marcos.v.silva@live.com>
2019-06-25 21:24:24 -0300

    Continue lab6

--- Laboratory6/Laboratory6.m
+++ Laboratory6/Laboratory6.m
@@ -1,1 +1,18 @@
-init
+%--------------------------------------------
+% Laboratório 6
+% Aluno: Marcos Vinicius Ribeiro Silva
+%--------------------------------------------
+
+% Adicionando o path onde contêm as imagens
+clc;
+addpath('Datasets');
+
+
+%--------------------------------------------
+% Exercício 1
+%--------------------------------------------
+
+% Dado os vetores x e y, calcule a distância euclidiana entre eles.
+
+x = [53 23 43 55 02 13];
+y = [53 23 44 55 02 13];
